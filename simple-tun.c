6793678897418294f3bf96f14723c3a4079202f4
Aniket Oak <aoak12@ufl.edu>
2013-07-25 16:10:38 -0400

    Multithreaded the app to handle read/writes from two devices faster

--- simple-tun.c
+++ simple-tun.c
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <netdb.h>
 #include <pwd.h>
+#include <pthread.h>
 
 
 
@@ -43,21 +44,6 @@ char prog_name[20];
 
 
 
-
-int mktun (char * , int , struct ifreq * );
-int settun (int , struct ifreq * , int );
-void read_bytes_tun (int , struct ifreq * );
-void tunnel (int, int);
-void check_usage (int, char * []);
-void print_usage ();
-void raise_error (const char *);
-int net_connect();
-int server_connect ();
-int client_connect ();
-
-
-
-
 struct user {
 	char uname[BUFF_SIZE];
 	struct passwd uinfo;
@@ -67,6 +53,7 @@ struct user {
 struct tun_dev {
 	char device[BUFF_SIZE];
 	int pers;
+	char ip_addr[60];
 };
 
 struct input {
@@ -80,6 +67,50 @@ struct input {
 	struct addrinfo server, * serv_ptr;		/* Address info of server (for client) */
 	struct user usr;
 } in;
+
+
+struct node {
+	struct node * next;
+	char packet[BUFF_SIZE];
+	int packet_len;
+};
+
+
+struct queue {
+	struct node * head;
+	struct node * tail;
+	int is_empty;
+	int num_ele;
+	pthread_mutex_t mutex;
+};
+
+
+struct thread_args {
+	int fd;
+	struct queue * n2t_queue, * t2n_queue;
+};
+
+
+
+
+
+
+int mktun (char * , int , struct ifreq * );
+int settun (int , struct ifreq * , int );
+void read_bytes_tun (int , struct ifreq * );
+void tunnel (int, int);
+void check_usage (int, char * []);
+void print_usage ();
+void raise_error (const char *);
+int net_connect();
+int server_connect ();
+int client_connect ();
+int push (struct queue * , char * , int );
+struct node * pop (struct queue * );
+struct queue * new_queue ();
+void * tun_io (void * );
+void * sock_io (void * );
+
 
 
 
@@ -222,8 +253,12 @@ int settun (int tun_fd, struct ifreq * ifr, int pers) {
 		/* Now set persistence */
 		if ( ioctl(tun_fd, TUNSETPERSIST, 1) < 0) 
 			raise_error("ioctl() - set persistence");
+
 	}
 
+/*	if (in.dev.ip_addr[0] != 0)
+		set_ip(tun_fd, ifr);*/
+	
 	printf("tun device: %s,",ifr->ifr_name);
 	if (owner != -1)
 		printf(" uid: %d,", owner);
@@ -234,7 +269,6 @@ int settun (int tun_fd, struct ifreq * ifr, int pers) {
 	free(buff);
 	return 0;
 }
-
 
 
 
@@ -288,139 +322,170 @@ void read_bytes_tun (int tun_fd, struct ifreq * ifr) {
 
 void tunnel (int sockfd, int tunfd) {
 
-	int lrgfd;
-	lrgfd = (sockfd > tunfd) ? sockfd : tunfd;		/* chose larger fd for select() */
+
+	pthread_t t2n, n2t;
+	int ret1, ret2;
+	struct queue * n2tq, * t2nq;
+	struct thread_args tun_to_net, net_to_tun;
+	
+
+	n2tq = new_queue();
+	t2nq = new_queue();
+
+	if (n2tq == NULL || t2nq == NULL)
+		raise_error("new_queue() failed");
+
+	net_to_tun.fd = sockfd;
+	net_to_tun.n2t_queue = n2tq;
+	net_to_tun.t2n_queue = t2nq;
+
+	tun_to_net.fd = tunfd;
+	tun_to_net.n2t_queue = n2tq;
+	tun_to_net.t2n_queue = t2nq;
+
+	printf("Starting the tunnelling threads\n");
+	ret1 = pthread_create( &t2n, NULL, tun_io, (void *) &tun_to_net);
+	ret2 = pthread_create( &n2t, NULL, sock_io, (void *) &net_to_tun);
+
+	pthread_join(t2n, NULL);
+	printf("Thread tun-to-network returned\n");
+	pthread_join(n2t, NULL);
+	printf("Thread network-to-tun returned\n");
+
+}
+
+
+
+
+
+
+
+void * sock_io (void * ptr) {
+
+
+	struct thread_args * args;
+	args = (struct thread_args *) ptr;
+
+	int fd;
+	struct queue * n2t, * t2n;
 	char buff[BUFF_SIZE];
-	int read_bytes, wrote_bytes;
-	int ret;			/* return value */
-	fd_set r_set;		/* set of file descriptors */
+	int stat = 0;
 	int len;
 
-	/* Following two structures are needed for storing client's address in server's case
-	NOTE that 'sockaddr_storage' is the only type that worked. sockaddr_in, sockaddr, etc
-	did not work. */
+	fd = args->fd;
+	n2t = args->n2t_queue;
+	t2n = args->t2n_queue;
+
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 10000;
+
+	fd_set r_set;
 	struct sockaddr_storage peer_addr;
 	socklen_t peer_addr_len = sizeof(peer_addr);
 
 
+	printf("Thread %ld: Starting operations on socket\n",pthread_self());
 	while (1) {
-
-		FD_ZERO(&r_set);			/* clear the set */
-		FD_SET(sockfd, &r_set);		/* Add the two descriptors to the set for read checking */
-		FD_SET(tunfd, &r_set);
-
-		/* Select will add the descriptor in the set if it is ready for a read. ISSET macro
-		is to be used to check if the descriptor is in the set */
-		ret = select(lrgfd+1, &r_set, NULL, NULL, NULL);
-
-		/* if select() returned an error because it caught a signal, we can try 
-		again */
-		if (ret < 0 && errno == EINTR)
-			continue;
-
-		/* is select() fails because of invalid descriptor or alloc failure, exit() */
-		if (ret < 0) 
-			raise_error("select() failed");
 		
+		/* Do pending operations from socket device to incoming queue */
+		FD_ZERO(&r_set);
+		FD_SET(fd, &r_set);
+		stat = select(fd+1, &r_set, NULL, NULL, &timeout);
+		if (stat < 0 && errno == EINTR);
+
+		if (stat < 0)
+			raise_error("select() failed");
 
 
-		/* Case where tun device is ready to be read */
+		if (FD_ISSET(fd, &r_set)) {
 
-		if (FD_ISSET(tunfd, &r_set)) {
-			
-			/* Read the data from tun device and write it on the network */
-			bzero(buff, BUFF_SIZE);
-			read_bytes = read(tunfd, buff, BUFF_SIZE);
-			if (read_bytes < 0)
-				raise_error("read() on tun failed");
-
-			if (in.over != SOCK_DGRAM) {
-				/* TCP makes the data appear as if it is a stream. Hence first we write the length 
-				of the data and then we write the packet itself. This helps at the receiver to 
-				distinguish between packets. (otherwise it will appear as single data stream and 
-				packet boundries can not be found). */
-				len = htons(read_bytes);
-				wrote_bytes = write(sockfd, &len, sizeof(len));
-				if (wrote_bytes < 0)
-					raise_error("write() on socket failed");
-			
-
-				wrote_bytes = write(sockfd, buff, read_bytes);
-				if (wrote_bytes < 0)
-					raise_error("write() on socket failed");
-
-			} else {
-				/* For UDP, we have to consider two cases */	
-				if (in.mode == 'c')
-					/* Client already has server's address, so no worries there. Just do a sendto() */
-					wrote_bytes = sendto(sockfd, buff, read_bytes, 0, in.serv_ptr->ai_addr, in.serv_ptr->ai_addrlen);
-				else if (in.mode == 's') 
-					/* Server does not have client's address from the start, so we have to use address stored
-					by recvfrom() call which must have happened some time before (server never initiates the 
-					conversation, so it is fair assumption that recvfrom() has happened before) */
-					wrote_bytes = sendto(sockfd, buff, read_bytes, 0, (struct sockaddr *) &peer_addr, peer_addr_len);
-				else
-					raise_error("invalid mode");
-
-
-
-				if (wrote_bytes < read_bytes)
-					raise_error("sendto() failed");
-			}
-			
-			if (in.verbose == 1)
-				printf("Read %d bytes from tun and wrote %d on socket\n", read_bytes, wrote_bytes);
-
-		}
-
-
-
-		/* Case where socket is ready to be read */
-
-		if (FD_ISSET(sockfd, &r_set)) {
-
-			/* Read the data from the network and write on tun device */
-			bzero(buff, BUFF_SIZE);
+			stat = 0;
 			len = 0;
+			bzero(buff, BUFF_SIZE);
 
-			/* First read the length of the packet */
-			if (in.over != SOCK_DGRAM) {
-				/* For TCP, life is simple.
-				First few bytes will be the length of the packet. */
-				read_bytes = read(sockfd, (char *) &len, sizeof(len));
-				if (read_bytes < 0)
-					raise_error("read() on socket failed");
+			if (in.over == SOCK_DGRAM) {
 
-				/* Now read the packet */
-				read_bytes = read(sockfd, buff, ntohs(len));
-				if (read_bytes < 0)
-					raise_error("read() on socket failed");
-			} else {
-				/* UDP has to have different behaviour for client and server */
 				if (in.mode == 'c')
-					/* For the client, we can ignore who we got the data from */
-					read_bytes = recvfrom(sockfd, buff, BUFF_SIZE, 0, NULL, NULL);
-				else if (in.mode == 's') 
-					/* Server has to store the address of the client to be used in a sendto() call
-					later */
-					read_bytes = recvfrom(sockfd, buff, BUFF_SIZE, 0, (struct sockaddr *) &peer_addr, &peer_addr_len);
+					stat = recvfrom(fd, buff, BUFF_SIZE, 0, NULL, NULL);
+				else if (in.mode == 's')
+					stat = recvfrom(fd, buff, BUFF_SIZE, 0, (struct sockaddr *) &peer_addr, &peer_addr_len);
 				else
 					raise_error("invalid mode");
-	
-				if (read_bytes < 0)
+
+				if (stat < 0)
 					raise_error("recvfrom() failed");
+
+
+			} else {
+
+				stat = read(fd, (char *) &len, sizeof(len));
+				if (stat < 0)
+					raise_error("read() failed on socket");
+
+				stat = read(fd, buff, ntohs(len));
+				if (stat < 0)
+					raise_error("read() failed on socket");
 			}
 
-			/* Write the packet on the tun device now */
-			wrote_bytes = write(tunfd, buff, read_bytes);
-			if (wrote_bytes < 0)
-				raise_error("write() on tun failed");
+			if (in.verbose == 1)
+				printf("Read %d bytes on socket\n",stat);
+
+			pthread_mutex_lock(&n2t->mutex);
+			
+			if (! push(n2t, buff, stat))
+				raise_error("push() failed in tun to network queue");
 
 			if (in.verbose == 1)
-				printf("Read %d bytes from socket and wrote %d on tun\n", read_bytes, wrote_bytes);
+				printf("n2t has %d packets\n",n2t->num_ele);
+
+			pthread_mutex_unlock(&n2t->mutex);
+
+
 		}
 
 
+
+		/* Write all the data on socket that needs to be written */
+		while (t2n->is_empty != 1) {
+
+			pthread_mutex_lock(&t2n->mutex);
+
+			struct node * n;
+			n = pop(t2n);
+			if (in.verbose == 1)
+				printf("t2n has %d more packets\n",t2n->num_ele);
+
+			pthread_mutex_unlock(&t2n->mutex);
+
+			if (in.over == SOCK_DGRAM) {
+
+				if (in.mode == 'c')
+					stat = sendto(fd, n->packet, n->packet_len, 0, in.serv_ptr->ai_addr, in.serv_ptr->ai_addrlen);
+				else if (in.mode == 's')
+					stat = sendto(fd, n->packet, n->packet_len, 0, (struct sockaddr *) &peer_addr, peer_addr_len);
+				else
+					raise_error("invalid mode");
+
+				if (stat < n->packet_len)
+					raise_error("sendto() - incomplete send");
+			} else {
+
+				len = htons(n->packet_len);
+				stat = write(fd, &len, sizeof(len));
+				if (stat < 0)
+					raise_error("write() failed on socket");
+
+				stat = write(fd, n->packet, n->packet_len);
+				if (stat < 0)
+					raise_error("write() failed on socket");
+			}
+
+
+			if (in.verbose == 1) 
+				printf("wrote %d bytes on socket\n",stat);
+			free(n);
+		}
 	}
 }
 
@@ -428,6 +493,91 @@ void tunnel (int sockfd, int tunfd) {
 
 
 
+void * tun_io (void * ptr) {
+
+	struct thread_args * args;
+	args = (struct thread_args *) ptr;
+
+	int fd;
+	struct queue * n2t, * t2n;
+	char buff[BUFF_SIZE];
+	int stat = 0;
+
+	fd = args->fd;
+	n2t = args->n2t_queue;
+	t2n = args->t2n_queue;
+
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 10000;
+
+	fd_set r_set;
+
+	printf("Thread %ld: Starting operations on tun\n",pthread_self());
+	while (1) {
+		
+		/* First write all the data on tun that needs to be written */
+
+		while (n2t->is_empty != 1) {
+
+			pthread_mutex_lock(&n2t->mutex);
+
+			struct node * n;
+			n = pop(n2t);
+			if (in.verbose == 1)
+				printf("n2t has %d more packets\n",n2t->num_ele);
+
+			pthread_mutex_unlock(&n2t->mutex);
+
+			stat = write(fd, n->packet, n->packet_len);
+			if (stat < 0)
+				raise_error("tun_io - write() failed");
+
+			if (in.verbose == 1)
+				printf("Wrote %d bytes on tun device\n",stat);
+
+			free(n);
+		}
+
+
+
+		/* Do pending operations from tun device to outgoing queue */
+		FD_ZERO(&r_set);
+		FD_SET(fd, &r_set);
+		stat = select(fd+1, &r_set, NULL, NULL, &timeout);
+		if (stat < 0 && errno == EINTR);
+
+		if (stat < 0)
+			raise_error("select() failed");
+
+
+		if (FD_ISSET(fd, &r_set)) {
+
+
+			stat = 0;
+			bzero(buff, BUFF_SIZE);
+			stat = read(fd, buff, BUFF_SIZE);
+			if (stat < 0)
+				raise_error("tun_io - read() failed");
+
+			if (in.verbose == 1)
+				printf("Read %d bytes on tun device\n",stat);
+
+			pthread_mutex_lock(&t2n->mutex);
+			
+			if (! push(t2n, buff, stat))
+				raise_error("push() failed in tun to network queue");
+
+			if (in.verbose == 1)
+				printf("t2n has %d more packets\n",t2n->num_ele);
+
+			pthread_mutex_unlock(&t2n->mutex);
+
+		}
+	}
+}
+
+	
 
 
 
@@ -466,6 +616,7 @@ void check_usage (int argc, char *argv[] ) {
 
 	bzero(in.serv, BUFF_SIZE);
 	bzero(in.dev.device, BUFF_SIZE);
+	bzero(in.dev.ip_addr, 60);
 	bzero(in.port_str, 10);
 	in.dev.pers = 0;
 	
@@ -574,24 +725,24 @@ void check_usage (int argc, char *argv[] ) {
 void print_usage () {
 	
 	printf("Usage: %s -m [mode] -d [device name] -p [port] -o [underlying prot] -s [server] -v \n\
-					  -m [mode] -d [device name] -e -u [user]\n\
+	            -m [mode] -d [device name] -e -u [user]\n\
 	where,\n\
-		-m:	mode		: either of 's' or 'c' signifying whether to act as client or server while tunnelling,\n\
-							OR\n\
-						  can be 'm' which tells the program that it is supposed to create new tun device.\n\
-		-d:	device name	: tun device name\n\
-		-p:	port		: port number used by client and server for tunnelling (for listening in case of server).\n\
-						  only significant in case mode isn't 'm'\n\
-		-o:	protocol	: name of the underlying protocol over which tunneling happens. can be 'tcp' or 'udp'\n\
-						  only significant in case mode isn't 'm'\n\
-		-s:	server name	: name or ip address of the server. (Only considered in case of client)\n\
-						  only significant in case mode isn't 'm'\n\
+		-m: mode        : either of 's' or 'c' signifying whether to act as client or server while tunnelling,\n\
+		                   OR\n\
+		                  can be 'm' which tells the program that it is supposed to create new tun device.\n\
+		-d: device name : tun device name\n\
+		-p: port        : port number used by client and server for tunnelling (for listening in case of server).\n\
+		                  only significant in case mode isn't 'm'\n\
+		-o: protocol    : name of the underlying protocol over which tunneling happens. can be 'tcp' or 'udp'\n\
+		                  only significant in case mode isn't 'm'\n\
+		-s: server name : name or ip address of the server. (Only considered in case of client)\n\
+		                  only significant in case mode isn't 'm'\n\
 		-e: persistence : Whether to set device persistent or not\n\
-						  only significant in case mode is 'm'\n\
-		-u: user		: User to set as owner of the device\n\
-						  only significant in case mode is 'm'\n\
-		-v: verbose		: print the info messages which may slow down the performance\n\
-		-h:	help		: print this usage\n", prog_name);
+		                  only significant in case mode is 'm'\n\
+		-u: user        : User to set as owner of the device\n\
+		                  only significant in case mode is 'm'\n\
+		-v: verbose     : print the info messages which may slow down the performance\n\
+		-h: help        : print this usage\n", prog_name);
 	
 	exit(0);
 }
@@ -757,4 +908,92 @@ int server_connect () {
 	} else {
 		return servsock;
 	}
+}
+
+
+
+
+
+
+
+
+/* ---------------------------------------------------------------------
+		QUEUE FUNCTIONS
+	probably should separate these in different file
+--------------------------------------------------------------------- */
+
+
+struct queue * new_queue () {
+
+	struct queue * q;
+	q = (struct queue *) malloc (sizeof(struct queue));
+
+	if (q == NULL)
+		return NULL;
+	
+
+	q->head = NULL;
+	q->tail = NULL;
+	q->is_empty = 1;
+	pthread_mutex_init(&q->mutex, NULL);
+	q->num_ele = 0;
+
+	return q;
+}
+
+
+
+
+
+
+
+int push (struct queue * q, char * buff, int len) {
+
+	struct node * n;
+	n = (struct node *) malloc (sizeof(struct node));
+
+	if (n == NULL)
+		return 0;
+	
+	bzero(n->packet, BUFF_SIZE);
+	bcopy(buff, n->packet, BUFF_SIZE);
+	n->packet_len = len;
+	n->next = NULL;
+
+	if (q->is_empty) {
+		
+		q->head = n;
+		q->tail = n;
+		q->is_empty = 0;
+
+	} else {
+	
+		q->tail->next = n;
+		q->tail = n;
+	}
+
+	q->num_ele++;
+
+	return 1;
+}
+
+
+
+struct node * pop (struct queue * q) {
+
+	if (q->is_empty)
+		return NULL;
+	
+	struct node * n;
+	n = q->head;
+
+	q->head = q->head->next;
+
+	if (q->head == NULL) {
+		q->tail = NULL;
+		q->is_empty = 1;
+	}
+	q->num_ele--;
+
+	return n;
 }
